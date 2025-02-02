f2028266b9daab8fb8e96b0a743da2cabae9dabe
devik-nangia <nangiadevik@gmail.com>
2025-02-02 12:04:42 +0530

    proxy and cors??

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(express.json({
 app.use(cookieParser())
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: '*',
   methods: ['GET', 'POST', 'PATCH'],
   credentials: true,
 }));
